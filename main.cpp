418b0e101e0631c143b4729b44e6a09b8866ed13
bittiboop <sstrelkova15@gmail.com>
2024-05-13 19:59:18 +0300

    added code for ex 1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main() {
-	cout << "Hello, World!" << endl;
+	int num1, num2, num3;
+	cin >> num1 >> num2;
+	for (int i=0; i <= num2; i++) {
+		cin >> num3;
+		if (num3 >= num1 && num3 <= num2) {
+			cout << "YES you get it";
+			break;
+		}
+		else {
+			cout << "NO you don't get it" << endl;
+			continue;
+		}
+	}
 	return 0;
 }
